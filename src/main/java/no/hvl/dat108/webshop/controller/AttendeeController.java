63fddf104de580ada32bc17dbfa679889d35c53d
h600895 <600895@stud.hvl.no>
2022-11-02 11:37:09 +0100

    #8

--- src/main/java/no/hvl/dat108/webshop/controller/AttendeeController.java
+++ src/main/java/no/hvl/dat108/webshop/controller/AttendeeController.java
@@ -1,15 +1,34 @@
 package no.hvl.dat108.webshop.controller;
 
+import no.hvl.dat108.webshop.util.LoginUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/${app.url.attendeeList}")
 public class AttendeeController {
 
+    @Value("/${app.url.login}")   private String LOGIN_URL;
+    @Value("/${app.message.requiresLogin}")   private String REQUIRES_LOGIN_MESSAGE;
+
+
     @GetMapping
-    public String getLoginView() {
+    public String showAttendees(HttpSession session, RedirectAttributes ra) {
+
+        if (!LoginUtil.erBrukerInnlogget(session)) {
+            ra.addFlashAttribute("redirectMessage", REQUIRES_LOGIN_MESSAGE);
+            return "redirect:" + LOGIN_URL;
+        }
+
+
+
+
+
         return "attendeeView";
     }
 	
