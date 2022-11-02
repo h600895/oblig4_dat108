44c2dd64a2a4846ce6c8bb32b7d0cfcfe4be16e5
600891 <89257225+600891@users.noreply.github.com>
2022-11-02 12:33:24 +0100

    Phone number changed to int and ID

--- src/main/java/no/hvl/dat108/webshop/model/Attendee.java
+++ src/main/java/no/hvl/dat108/webshop/model/Attendee.java
@@ -1,11 +1,53 @@
 package no.hvl.dat108.webshop.model;
 
+import javax.persistence.Id;
+
 public class Attendee {
     private String firstName;
     private String lastName;
-    private String phone;
+
+    @Id
+    private int phone;
     private String password;
     private String gender;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
     public Attendee(String firstName, String lastName, String phone, String gender) {
         this.firstName = firstName;
@@ -23,19 +65,10 @@ public class Attendee {
         this.gender = "Mann";
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public String getGender() {
-        return gender;
-    }
 
-    public String getLastName() {
-        return lastName;
-    }
 
-    public String getPhone() {
-        return phone;
-    }
+
+
+
 }
