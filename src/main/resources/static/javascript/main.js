dd9830007497c392bdb280884c2339938df4b579
h600895 <600895@stud.hvl.no>
2022-11-09 18:23:21 +0100

    .

---
+++ src/main/resources/static/javascript/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const validity = document.getElementById("pword")
+    validity.addEventListener('input', sjekkPassordStyrke);
+
+function sjekkPassordStyrke() {
+    if (validity.valid) {
+       if( validity.length <12) {
+           //skru på mediumpassord
+       }
+    }
+}
